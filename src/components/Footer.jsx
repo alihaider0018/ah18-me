6ac09d0bf8777cfd42b22abb10ca9e86c47091bf
alihaider0018 <ah18contact@gmail.com>
2024-08-15 12:47:37 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import "../styles/Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="Footer">
+      <h4>Copyright © 2024. All rights are reserved</h4>
+
+      <div className="socials">
+        <a href="https://www.linkedin.com/in/alihaider0018/">
+          <i class="ri-linkedin-box-line"></i>
+        </a>
+        <a href="https://github.com/alihaider0018">
+          <i class="ri-github-line"></i>
+        </a>
+      </div>
+    </footer>
+  );
+}
---
+++ src/components/Home.jsx
@@ -0,0 +1,41 @@
+import "../styles/Home.css";
+
+export default function Home() {
+  return (
+    <section className="Home" id="home">
+      <div className="main">
+        <div className="main-text-area">
+          <h1>
+            Full-Stack <br /> Developer
+          </h1>
+          <p>
+            Hi, I am Ali Haider. A passionate Full-Stack Developer based in New
+            Delhi, India.
+          </p>
+          <span className="icons">
+            <a href="https://www.linkedin.com/in/alihaider0018/">
+              <i class="ri-linkedin-box-line"></i>
+            </a>
+            <a href="https://github.com/alihaider0018">
+              <i class="ri-github-line"></i>
+            </a>
+          </span>
+        </div>
+        <div className="main-img-area">
+          <img src="/images/profilepic.jpeg" alt="profile" />
+        </div>
+      </div>
+
+      <div className="skills">
+        <h3>Tech Stack</h3>
+        <p>|</p>
+        <div className="skills-icon">
+          <img src="https://skillicons.dev/icons?i=html,css,js" />
+        </div>
+        <div className="skills-icon">
+          <img src="https://skillicons.dev/icons?i=react,mongodb,express" />
+        </div>
+      </div>
+    </section>
+  );
+}
